554093f6a05120fcd4e26d9729482232dd8c1569
MashaGarkaeva <77572567+MashaGarkaeva@users.noreply.github.com>
2021-12-15 08:35:37 +0300

    Add files via upload

---
+++ app/src/main/java/com/example/team_cs_go/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.team_cs_go;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ListView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_first);
+    }
+}
